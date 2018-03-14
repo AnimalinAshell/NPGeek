b0d443695c2b066d52b5f76f6ae86f4e0c322532
Rob Roy <r.m.royiii@gmail.com>
2018-03-14 09:21:38 -0400

    Home Controller, Views, Connectionstring

---
+++ NPGeek.Web/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NPGeek.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult HomePage()
+        {
+            return View("HomePage");
+        }
+
+        public ActionResult DetailPage()
+        {
+            return View("DetailPage");
+        }
+
+        public ActionResult Survey()
+        {
+            return View("Survey");
+        }
+
+        public ActionResult FavoriteParks()
+        {
+            return View("FavoriteParks");
+        }
+
+    }
+}
